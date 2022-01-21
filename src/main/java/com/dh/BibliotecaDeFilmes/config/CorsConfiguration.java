ac328f8a76794485c733830b386a59b7efd799e9
LucasPagliaDH <lgrisa@digitalhouse.com>
2022-01-21 20:51:49 -0300

    CorsCongig

---
+++ src/main/java/com/dh/BibliotecaDeFilmes/config/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.dh.BibliotecaDeFilmes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+
+        registry.addMapping("/filmes/**")
+                .allowedOrigins("http://localhost:4000")
+                .allowedMethods("GET", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
