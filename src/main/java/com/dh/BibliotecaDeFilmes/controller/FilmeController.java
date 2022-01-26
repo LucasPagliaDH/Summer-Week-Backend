5c05987a584fc5281dd81c5400d7a3e35356e5ef
LucasPagliaDH <lgrisa@digitalhouse.com>
2022-01-26 14:55:54 -0300

    fixando o cors e adicionando o metodo PUT

--- src/main/java/com/dh/BibliotecaDeFilmes/controller/FilmeController.java
+++ src/main/java/com/dh/BibliotecaDeFilmes/controller/FilmeController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/filme")
+@CrossOrigin(origins = "*", allowedHeaders = "")
 public class FilmeController {
 
     @Autowired
@@ -16,6 +17,11 @@ public class FilmeController {
 
     @PostMapping
     public Filme post(@RequestBody Filme filme){
+        return service.create(filme);
+    }
+
+    @PutMapping
+    public Filme put(@RequestBody Filme filme){
         return service.create(filme);
     }
 
